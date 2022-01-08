be45ff31eee2cf95445e6f636cb6c3fa8e8fda5e
Khan M Faisal Nafis <iamfaisalnafis@gmail.com>
2022-01-09 04:14:51 +0530

    Update

--- antonandletters.cpp
+++ antonandletters.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
 #include <set>
 
 using namespace std;
 
 int main()
 {
-    set<int> s1{};
-    
+    string s;
+    set<char> a;
+    getline(cin, s);
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s[i] >= 'a' && s[i] <= 'z')
+        {
+            a.insert(s[i]);
+        }
+    }
+    cout << a.size() << endl;
+    return 0;
 }
