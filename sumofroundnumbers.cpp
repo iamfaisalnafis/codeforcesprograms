69fe6a335c82f25ca5e8fbbfc1af058701d3133f
Khan M Faisal Nafis <iamfaisalnafis@gmail.com>
2022-01-12 22:05:54 +0530

    Update

---
+++ sumofroundnumbers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[6];
+        int total(0), m(1);
+        for(int i = 1; i < 6; i++)
+        {
+            arr[i] = n % 10;
+            n /= 10;
+            if(arr[i] != 0)
+            {
+                total++;
+            }
+        }
+        cout << total << endl;
+        for(int i = 1; i < 6; i++)
+        {
+            if(arr[i] != 0)
+            {
+                cout << arr[i] * m << " ";
+            }
+            m = m * 10;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
